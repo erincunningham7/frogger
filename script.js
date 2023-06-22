9b01247e95cc2f2d4d6f4040aa61da5a1cb302ab
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-22 11:52:40 +0100

    Add if statements to keep frog in the grid in script.js

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ function moveFrog(e) {
     squares[currentIndex].classList.remove('frog')
     switch(e.key) {
         case 'ArrowLeft':
-            currentIndex -= 1
+            if (currentIndex % width !== 0)
+                currentIndex -= 1
             break
         case 'ArrowRight':
+            if (currentIndex % width < width - 1)
             currentIndex += 1
             break
         case 'ArrowUp':
+            if (currentIndex - width >= 0)
             currentIndex -= width
             break
         case 'ArrowDown':
+            if (currentIndex + width < width * width)
             currentIndex += width
             break
     }
