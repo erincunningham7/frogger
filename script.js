999d07ab3e3f98c28ae8aed8bafc0c5aa0165338
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-22 11:40:00 +0100

    Use current index to move frog in script.js

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 console.log(squares)
 let currentIndex = 76
+const width = 9
 
 function moveFrog(e) {
     switch(e.key) {
         case 'ArrowLeft':
-            console.log('move left')
+            currentIndex -= 1
             break
         case 'ArrowRight':
-            console.log('move right')
+            currentIndex += 1
             break
         case 'ArrowUp':
-            console.log('move up')
+            currentIndex -= width
             break
         case 'ArrowDown':
-            console.log('move down')
+            currentIndex += width
             break
     }
 
