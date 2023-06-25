6d034cd51039d726ebd62f3e1d12df24fc7b3f48
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-25 22:14:49 +0100

    Move right logs in script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 const logsLeft = document.querySelectorAll('.log-left')
+const LogsRight = document.querySelectorAll('.log-right')
 console.log(squares)
 let currentIndex = 76
 const width = 9
@@ -35,6 +36,7 @@ document.addEventListener('keyup', moveFrog)
 
 function autoMoveLogs() {
     logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    LogsRight.forEach(logRight => moveLogRight(logRight))
 }
 
 //autoMoveLogs()
@@ -60,6 +62,31 @@ function moveLogLeft(logLeft) {
         case logLeft.classList.contains('l5') :
             logLeft.classList.remove('l5')
             logLeft.classList.add('l1')
+            break
+    }
+}
+
+function moveLogRight(logRight) {
+    switch(true) {
+        case logRight.classList.contains('l1') :
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+        case logRight.classList.contains('l2') :
+            logRight.classList.remove('l2')
+            logRight.classList.add('l1')
+            break
+        case logRight.classList.contains('l3') :
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break
+        case logRight.classList.contains('l4') :
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break
+        case logRight.classList.contains('l5') :
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
             break
     }
 }
