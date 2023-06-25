b2e0871e9cdaec47ceb26fc04083366aa5bab2c0
Erin Cunningham <cunninnghamerin@gmail.com>
2023-06-25 22:07:39 +0100

    Move logs in script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
+const logsLeft = document.querySelectorAll('.log-left')
 console.log(squares)
 let currentIndex = 76
 const width = 9
@@ -31,3 +32,36 @@ function moveFrog(e) {
 
 }
 document.addEventListener('keyup', moveFrog)
+
+function autoMoveLogs() {
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+}
+
+//autoMoveLogs()
+
+function moveLogLeft(logLeft) {
+    switch(true) {
+        case logLeft.classList.contains('l1') :
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
+            break
+        case logLeft.classList.contains('l2') :
+            logLeft.classList.remove('l2')
+            logLeft.classList.add('l3')
+            break
+        case logLeft.classList.contains('l3') :
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
+            break
+        case logLeft.classList.contains('l4') :
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
+            break
+        case logLeft.classList.contains('l5') :
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
+            break
+    }
+}
+
+setInterval(autoMoveLogs, 1000)
